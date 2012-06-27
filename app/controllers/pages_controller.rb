95eea249a403554c8d1ff2ef02aa10d3f4139a48
Robert Chuchro <rchuchro@groupon.com>
2012-06-27 11:15:45 -0500

    pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,8 @@ class PagesController < ApplicationController
     @page_url = SITE_URL
 
     if (params[:code])
-      session[:access_token] = session[:oauth].get_access_token(params[:code])
+      session[:oauth]
+      #session[:access_token] = session[:oauth].get_access_token(params[:code])
       #@api = Koala::Facebook::API.new(session[:access_token]) 
       #@graph_data = @api.get_object("/me/statuses")
       #@api.put_connections("me", "faiceupon:buy", :groupon => @page_url)
