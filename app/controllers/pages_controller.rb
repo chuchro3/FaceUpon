b7ed5b192bb55b21390e191701d0f174d31f0ef7
Robert Chuchro <rchuchro@groupon.com>
2012-06-27 09:48:36 -0500

    pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,15 +6,6 @@ class PagesController < ApplicationController
     @page_url = SITE_URL + "/pages/callback" 
 
 
-    if (params[:code])
-      print "hi"
-      session[:access_token] = session[:oauth].get_access_token(params[:code])
-      @api = Koala::Facebook::API.new(session[:access_token]) 
-      @graph_data = @api.get_object("/me/statuses")
-      @api.put_connections("me", "faiceupon:buy", :groupon => @page_url+"?code="+params[:code])
-    
-
-    end
 
   end
 
@@ -22,8 +13,16 @@ class PagesController < ApplicationController
     @title = "home"
     @page_url = SITE_URL
 
-    session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL+'/pages/callback')
+    session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, @page_url)
     @auth_url = session[:oauth].url_for_oauth_code(:permissions=>"read_stream")    
+
+    if (params[:code])
+      session[:access_token] = session[:oauth].get_access_token(params[:code])
+      @api = Koala::Facebook::API.new(session[:access_token]) 
+      @graph_data = @api.get_object("/me/statuses")
+      @api.put_connections("me", "faiceupon:buy", :groupon => @page_url+"?code="+params[:code])
+    end
+
   end
 
   def facebook_action
