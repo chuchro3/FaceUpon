e63e8f9971f4bb076c6129be1dac2f09e24c6e3e
Robert Chuchro <rchuchro@groupon.com>
2012-06-27 16:18:47 -0500

    test

--- app/controllers/groupon_deal_controller.rb
+++ app/controllers/groupon_deal_controller.rb
@@ -7,7 +7,7 @@ class GrouponDealController < ApplicationController
 
   def viewdeal
     @groupon = GrouponDeal.find(params[:id])    
-    @page_url = SITE_URL + "/groupon_deal/" + params[:id] + "/"
+    @page_url = SITE_URL + "/groupon_deal/" + params[:id]
     if (!session.nil?) 
       session[:api].put_connections("me", "faiceupon:view", :groupon => @page_url)
     end
