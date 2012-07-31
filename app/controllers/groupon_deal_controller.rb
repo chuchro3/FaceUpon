6de4ef067a7181d44db1ae58b671c8a3ad448d15
Robert Chuchro <rchuchro@groupon.com>
2012-07-31 17:24:25 -0500

    updated search display/functionality

--- app/controllers/groupon_deal_controller.rb
+++ app/controllers/groupon_deal_controller.rb
@@ -31,7 +31,7 @@ class GrouponDealController < ApplicationController
 
       @groupon_deal = GrouponDeal.find_with_index('^'+params[:search])
       @groupon_deal = @groupon_deal.select {|x| x[:active_status]} | @groupon_deal.reject {|x| x[:active_status]}
-      @groupon_deal = @groupon_deal.select {|x| x.division_name == params[:division]}
+      @groupon_deal = @groupon_deal.select {|x| x.division_name == params[:division]} unless params[:division].empty?
       @groupon_deal = @groupon_deal.paginate(:page => params[:page], :per_page => 10)
       
       if @groupon_deal.empty?
